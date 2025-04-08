2f5a1d90763fe7d32b0aa5f6eed8656f3d2eef27
Franco Cipollone <franco.c@ekumenlabs.com>
2025-04-08 17:17:25 -0400

    Relax constraints to avoid cartesian reflex.

--- franka_hardware/src/robot.cpp
+++ franka_hardware/src/robot.cpp
@@ -41,6 +41,24 @@ Robot::Robot(const std::string& robot_ip, const rclcpp::Logger& logger) {
   robot_ = std::make_unique<franka::Robot>(robot_ip, rt_config);
   model_ = std::make_unique<franka::Model>(robot_->loadModel());
   franka_hardware_model_ = std::make_unique<Model>(model_.get());
+  // Set the collision behavior.
+  std::array<double, 7> lower_torque_thresholds_nominal{
+    {25.0, 25.0, 22.0, 20.0, 19.0, 17.0, 14.}};
+  std::array<double, 7> upper_torque_thresholds_nominal{
+      {35.0, 35.0, 32.0, 30.0, 29.0, 27.0, 24.0}};
+  std::array<double, 7> lower_torque_thresholds_acceleration{
+      {25.0, 25.0, 22.0, 20.0, 19.0, 17.0, 14.0}};
+  std::array<double, 7> upper_torque_thresholds_acceleration{
+      {35.0, 35.0, 32.0, 30.0, 29.0, 27.0, 24.0}};
+  std::array<double, 6> lower_force_thresholds_nominal{{30.0, 30.0, 30.0, 25.0, 25.0, 25.0}};
+  std::array<double, 6> upper_force_thresholds_nominal{{40.0, 40.0, 40.0, 35.0, 35.0, 35.0}};
+  std::array<double, 6> lower_force_thresholds_acceleration{{30.0, 30.0, 30.0, 25.0, 25.0, 25.0}};
+  std::array<double, 6> upper_force_thresholds_acceleration{{40.0, 40.0, 40.0, 35.0, 35.0, 35.0}};
+  robot_->setCollisionBehavior(
+      lower_torque_thresholds_acceleration, upper_torque_thresholds_acceleration,
+      lower_torque_thresholds_nominal, upper_torque_thresholds_nominal,
+      lower_force_thresholds_acceleration, upper_force_thresholds_acceleration,
+      lower_force_thresholds_nominal, upper_force_thresholds_nominal);
 }
 
 Robot::~Robot() {
